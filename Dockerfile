0b1dbb02ee9849c3de6a1868732b2028ec097e7d
Tad Hatch <tadhatch35@hotmail.com>
2024-05-30 11:07:23 -0400

    testing selenium webdriver for onedrive support

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
 FROM python:3.12
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+RUN apt-get update
+RUN apt-get install -y google-chrome-stable
+RUN apt-get install -y unzip
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+ENV DISPLAY=:99
 COPY webapp.py /app/
 COPY gunicorn_config.py /app/
 COPY requirements.txt /
--- webapp.py
+++ webapp.py
@@ -22,6 +22,32 @@ def gfg():
     titles = [x.text for x in soup.find_all('div', class_="tyTrke")]
     return titles, links
 
+  def get_link_from_onedrive(url):
+    s = get_session()
+    s.get(url)
+    soup = bs4(s.page_source, "html.parser")
+    print(soup)
+    title = soup.title.text
+    print(title)
+    link = soup.find("img")['src']
+    print(link)
+    return title, link
+
+  def get_session():
+    print("i'm in session()")
+    from selenium import webdriver
+    print("got selenium")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"')
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_experimental_option('useAutomationExtension', False)
+    print(options, "options")
+    driver = webdriver.Chrome(options=options)
+    print(driver, 'built')
+    return driver
+
   def generate_qr_codes(filelist):
     qr_links = []
     for link in filelist:
@@ -44,7 +70,14 @@ def gfg():
   if request.method == "POST":
     try:
       url = request.form.get("files") 
-      titles, links = get_links_from_drive(url)
+      print(url)
+      if "drive.google.com" in url:
+        titles, links = get_links_from_drive(url)
+      elif "1drv.ms" in url:
+        print("onedrive link")
+        titles, links = get_link_from_onedrive(url)
+      else:
+        raise NameError("Invalid URL")
       qr_links = generate_qr_codes(links)
       return render_template("links.html", links=zip(titles, qr_links))
     except Exception as e:
